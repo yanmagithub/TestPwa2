611c960143b99864b3be89b372e15efe786e3956
yanmagithub <yanma121@gmail.com>
2018-12-05 04:12:14 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.8f36bc4e15a536d6b2c09d8d7e3eade6.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "08ef61ad9d72e45723401f1ee33e3c39"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "ba281bd88aa86a640c771404ba28e3db"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
